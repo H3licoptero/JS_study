5a2b3cb520cec4643c13ce69d69c444d9f350563
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-01-24 18:57:20 +0300

    ДЗ ко второму уроку

--- script/script.js
+++ script/script.js
@@ -1,1 +1,39 @@
-console.log('Hello World!');
+let money = 60000;
+
+let income = 60000;
+
+let addExpenses = "Жилье, транспорт, одежда, техника";
+
+let deposit = true;
+
+let mission = 100000;
+
+let period = 10;
+
+/* 
+ Вот так, как я понял через одно ключевое слово и в столбик.
+
+ let money = 60000,
+ income = 60000,
+ addExpenses = 'Жилье, транспорт, техника, одежда',
+ deposit = true,
+ mission = 100000,
+ period = 12;
+ */
+
+console.log(typeof money, typeof income, typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log(
+  "Период равен " +
+    period +
+    "-ти" +
+    " месяцам. \nЦель заработать " +
+    mission +
+    "$."
+);
+
+let budgetDay = 330;
+
+console.log(budgetDay);
