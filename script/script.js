d5b490319b3b0503c77c1b143d92f3d188903a60
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-01-24 18:54:29 +0300

    ДЗ к первому уроку

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-console.log('Hello World!');
+// let  money,
+//      income,
+//      addExpenses,
+//      deposit,
+//      mission,
+//      period;
+// это за вторым разом ибо в первый я аццки накосячил
+// и пришлось удалить весь репозиторий и создать новый.
+
+let money;
+let income;
+let addExpenses;
+let deposit;
+let mission;
+let period;
+
+alert("Hello World!");
+console.log("My first repository.");
