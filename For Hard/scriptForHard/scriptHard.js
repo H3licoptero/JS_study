f9806576d64beb30c7f75f3256995c0095c48ccb
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-01-28 00:04:34 +0300

    Для усложнённого задания hard03

---
+++ For Hard/scriptForHard/scriptHard.js
@@ -0,0 +1,24 @@
+'use strict';
+
+let lang = prompt('', '');
+
+    if (lang === 'ru') {
+        alert('Понедельник, вторник, среда четверг, пятница');
+    } else if (lang === 'en') {
+        alert('Monday, tuesday, wednesday, thursday, friday');
+    }
+
+    switch (lang) {
+        case 'ru':
+             alert("Понедельник, вторник, среда четверг, пятница");
+             break;
+        case 'en':
+            alert("Monday, tuesday, wednesday, thursday, friday");
+            break;
+    }
+
+    let person = prompt('Введите имя ', ' ');
+    let namePerson = ( person === 'Артём') ? console.log("Директор") :
+     (person === 'Максим') ? console.log('Преподаватель') : console.log('студент');
+
+    
