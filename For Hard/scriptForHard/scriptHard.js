c6bf7c41a685223a9f9caa7747f98ec6a929fff2
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-01-29 00:07:33 +0300

    Четвёртый хард

--- For Hard/scriptForHard/scriptHard.js
+++ For Hard/scriptForHard/scriptHard.js
@@ -1,26 +1,15 @@
 'use strict';
 
-let lang = prompt('', '');
-
-
-    if (lang === 'ru') {
-        alert('Понедельник, вторник, среда четверг, пятница');
-    } else if (lang === 'en') {
-        alert('Monday, tuesday, wednesday, thursday, friday');
+ function takeInArg(arg) {
+    if (arg !== String()) {
+         alert('Вводить можно только строки.');
+    } else if (arg.length > 30) {
+        console.log(takeInArg.trim());
+    } else if (arg.length > 30) {
+        console.log(arg.substr(0, 30) + '...');
     }
+}
 
-    switch (lang) {
-        case 'ru':
-             alert("Понедельник, вторник, среда четверг, пятница");
-             break;
-        case 'en':
-            alert("Monday, tuesday, wednesday, thursday, friday");
-            break;
-    }
-// Тут ещё нужен многомерный массив
 
-let person = prompt("Введите имя ", " ");
-    let namePerson = ( person === 'Артём') ? console.log("Директор") :
-     (person === 'Максим') ? console.log('Преподаватель') : console.log('студент');
-
-    
+let result = prompt('', '');
+console.log();
