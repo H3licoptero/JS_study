75881d60bb9e4edccf37242a11043fcc85bb0485
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-02-04 22:25:14 +0300

    ДЗ к девятому уроку

---
+++ script/indexScript/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+let calculate = document.getElementById('start');
+console.log(calculate);
+
+let incomeBtn = document.getElementsByTagName('button')[0];
+console.log(incomeBtn);
+
+let expnesesBtn = document.getElementsByTagName('button')[1];
+console.log(expnesesBtn);
+
+let depositCheck = document.querySelector('#deposit-check');
+console.log(depositCheck);
+
+let additionalIncomeItem = document.querySelectorAll('.additional_income')[0];
+console.log(additionalIncomeItem);
+
+let values = document.querySelectorAll('.result');
+console.log(values);
--- script/script.js
+++ script/script.js
@@ -157,3 +157,20 @@ console.log(appData);
 appData.getInfoDeposit();
 console.log(appData.calcSavedMoney(), appData.moneyDeposit, appData.percentDeposit);
 
+let calculate = document.getElementById('start');
+console.log(calculate);
+
+let incomeBtn = document.getElementsByTagName('button')[0];
+console.log(incomeBtn);
+
+let expnesesBtn = document.getElementsByTagName('button')[1];
+console.log(expnesesBtn);
+
+let depositCheck = document.querySelector('#deposit-check');
+console.log(depositCheck);
+
+let additionalIncomeItem = document.querySelectorAll('.additional_income')[0];
+console.log(additionalIncomeItem);
+
+let values = document.querySelectorAll('.result');
+console.log(values);
