c7bb2db5fe071e83d0c474eb108876e3734188d1
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-02-16 17:46:36 +0300

    ДЗ к четрырнадцатому уроку первое

--- урок 14/script/script.js
+++ урок 14/script/script.js
@@ -1,81 +1,54 @@
 "use strict";
 
 function DomElement(selector, height, width, bg, fontSize) {
-
   this.selector = selector;
   this.height = height;
   this.width = width;
   this.bg = bg;
   this.fontSize = fontSize;
-
 }
 
 DomElement.prototype.createElem = function() {
-  
-  let createElement = document.querySelector(".create"), // это инпут, где вводится сообщение
-    enter = document.querySelector(".enter"); // это конпка ввода
+  if(this.selector.charAt(0) === '.') {
+     let item = document.createElement('div');
+     item.classList.add(this.selector.substring(1));
+     item.className = 'block';
+     item.style.height = this.height;
+     item.style.width = this.width;
+     item.style.background = this.bg;
+     item.style.fontSize = this.fontSize;
+     item.textContent = this.selector.substring(1);
+     document.body.append(item);
 
-  enter.addEventListener("click", function() {
+     console.log(item);
+  } else if(this.selector.charAt(0) === '#') {
+     let itemTwo = document.createElement("p");
+     itemTwo.classList.add(this.selector.substring(1));
+     itemTwo.className = 'best';
+     itemTwo.style.height = this.height;
+     itemTwo.style.width = this.width;
+     itemTwo.style.background = this.bg;
+     itemTwo.style.fontSize = this.fontSize;
+     itemTwo.textContent = this.selector.substring(1);
+     document.body.append(itemTwo);
 
-    let elementDiv = document.createElement("div"); //создаем div
-    let elementParagraph = document.createElement("p"); // создаем параграф
+     console.log(itemTwo);
+  } else {
+    alert('Ничего не создано!');
+  }
 
-    this.selector = createElement;
-    if (this.selector.value.charAt(0) === ".") {
-      elementDiv.className = "block";
-      elementDiv.style.height = this.height;
-      elementDiv.style.width = this.width;
-      elementDiv.style.backgroundColor = this.bg;
-      elementDiv.style.fontSize = this.fontSize;
-      elementDiv.textContent = this.selector.value.slice(1);
-      document.body.append(elementDiv);
-
-    } else if (this.selector.value.charAt(0) === "#") {
-      elementParagraph.className = "best";
-      elementParagraph.style.height = this.height;
-      elementParagraph.style.width = this.width;
-      elementParagraph.style.backgroundColor = this.bg;
-      elementParagraph.style.fontSize = this.fontSize;
-      elementParagraph.textContent = this.selector.value.slice(1);
-      document.body.append(elementParagraph);
-    }
-  });
-
-  
 };
 
- let element = new DomElement(".div", "100px", "200px", "beige", "20px");
+ let input = document.querySelector(".create"),
+   button = document.querySelector(".enter"),
+   string;
 
+button.addEventListener('click', function() {
+   let string = input.value;
+   input.value = '';
+   let element = new DomElement(string, "150px", "300px", "gold", "30px");
+   element.createElem();
+});
 
-DomElement.prototype.createElem(element);
-
-
-
-// console.log(DomElement(element));
-
-
-
-// let createElement = document.querySelector(".create"),
-//   enter = document.querySelector(".enter");
-
-// enter.addEventListener("click", function() {
-//   let newEl = document.createElement("p");
-//   let newDiv = document.createElement("div");
-
-//   if (createElement.value.charAt(0) === ".") {
-//     newDiv.className = "block";
-//     newDiv.style.backgroundColor = 'beige';
-//     newDiv.style.fontSize = '30px';
-//     newDiv.textContent = createElement.value.slice(1);
-//     document.body.append(newDiv);
-
-//   } else if (createElement.value.charAt(0) === "#") {
-//     newEl.className = "best";
-//     newEl.style.backgroundColor = "gold";
-//     newEl.style.fontSize = "30px";
-//     newEl.textContent = createElement.value.slice(1);
-//     document.body.append(newEl);
-//   }
-// });
 
 
