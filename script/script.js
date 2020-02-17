fae781edee0914e494084841ec023d5b63f54414
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-02-17 17:35:32 +0300

    ДЗ к пятнадцатому уроку

--- script/script.js
+++ script/script.js
@@ -23,8 +23,12 @@ let salaryAmount = document.querySelector(".salary-amount"),
 let budgetMonth = document.getElementsByClassName("budget_month-value")[0],
   budgetDay = document.getElementsByClassName("budget_day-value")[0],
   expensesMonth = document.getElementsByClassName("expenses_month-value")[0],
-  additionalIncome = document.getElementsByClassName("additional_income-value")[0],
-  additionalExpensesValue = document.getElementsByClassName("additional_expenses-value")[0],
+  additionalIncome = document.getElementsByClassName(
+    "additional_income-value"
+  )[0],
+  additionalExpensesValue = document.getElementsByClassName(
+    "additional_expenses-value"
+  )[0],
   incomePeriodValue = document.getElementsByClassName("income_period-value")[0],
   targetMonth = document.getElementsByClassName("target_month-value")[0];
 
@@ -33,7 +37,8 @@ let isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-const AppData = function() {
+class AppData {
+    constructor(){
   this.income = {};
   this.incomeMonth = 0;
   this.addIncome = [];
@@ -46,9 +51,9 @@ const AppData = function() {
   this.budgetDay = 0;
   this.budgetMonth = 0;
   this.expensesMonth = 0;
-};
+}
 
-AppData.prototype.start = function() {
+  start() {
   // блокировка рассчёта при пустых полях
   if (salaryAmount.value === "") {
     return;
@@ -77,76 +82,77 @@ AppData.prototype.start = function() {
   this.getBudget();
 
   this.showResult();
-};
+}
 
 /* изменения значений при использовании скролла periodSelect,
   динамическое изменение "накоплений за период" после расчёта */
-AppData.prototype.calcPeriod = function() {
+calcPeriod() {
   incomePeriodValue.value = salaryAmount.value * periodSelect.value;
   periodAmount.textContent = periodSelect.value;
-};
+}
 
 //метод для вывода всех наших рассчётов в поля всего блока result(html)
-AppData.prototype.showResult = function() {
-  let _this = this;
+showResult() {
   budgetMonth.value = this.budgetMonth;
   budgetDay.value = this.budgetDay;
   expensesMonth.value = this.expensesMonth;
-  additionalExpensesValue.value = _this.addExpenses.join(", ");
-  additionalIncome.value = _this.addIncome.join(", ");
+  additionalExpensesValue.value = this.addExpenses.join(", ");
+  additionalIncome.value = this.addIncome.join(", ");
   targetMonth.value = Math.ceil(this.getTargetMonth());
   incomePeriodValue.value = this.calcSavedMoney();
-  periodSelect.addEventListener("change", _this.calcPeriod());
-};
+  periodSelect.addEventListener("change", () => {
+      this.calcPeriod();
+  });
+}
 
-AppData.prototype.getExpensesMonth = function() {
+//запись расходов в наш объект expenses
+getExpensesMonth() {
   for (let key in this.expenses) {
     this.expensesMonth += this.expenses[key];
   }
-};
+}
 
 //тут рассчитывается месячный доход с учётом расходов(budgetMonth), а так же расчитывается доход на день
-AppData.prototype.getBudget = function() {
+getBudget() {
   this.budgetMonth = +this.budget + this.incomeMonth - this.expensesMonth;
   this.budgetDay = Math.ceil(this.budgetMonth / 30);
-};
+}
 
 //рассчитываем возможный доход запись в поля "additional_income-item"
-AppData.prototype.getAddIncome = function() {
-  additionalIncomeItem.forEach((item) => {
+getAddIncome() {
+  additionalIncomeItem.forEach(item => {
     let itemValue = item.value.trim();
     if (itemValue !== "") {
       this.addIncome.push(itemValue);
     }
   });
-};
+}
 
 //записываем наши возможные расходы в поле "возможные расходы"(additional_expenses-item)
-AppData.prototype.getAddExpenses = function() {
+getAddExpenses() {
   let additionalExpensesAdd = additionalExpensesItem.value.split(",");
-  
-  additionalExpensesAdd.forEach((item) => {
+
+  additionalExpensesAdd.forEach(item => {
     item = item.trim();
     if (item !== "") {
       this.addExpenses.push(item);
     }
   });
-};
+}
 
 //добавляем поля для наших возможных доходов
-AppData.prototype.addIncomeBlock = function() {
+addIncomeBlock() {
   let incomeItemClone = incomeItems[0].cloneNode(true);
   incomeItems[0].parentNode.insertBefore(incomeItemClone, incomeBtn);
   incomeItems = document.querySelectorAll(".income-items");
   if (incomeItems.length === 3) {
     incomeBtn.style.display = "none";
   }
-};
+}
 
 // рассчитываем доп.доход в модальных окнах
-AppData.prototype.getIncome = function() {
-
-  incomeItems.forEach((item) => {
+getIncome() {
+  incomeItems.forEach(item => {
     let itemsIncome = item.querySelector(".income-title").value;
     let cashItems = item.querySelector(".income-amount").value;
 
@@ -157,37 +163,36 @@ AppData.prototype.getIncome = function() {
   for (const key in this.income) {
     this.incomeMonth += +this.income[key];
   }
-};
+}
 
 //блок для занаесения значений в поле обязательных расходов(expenses)
-AppData.prototype.addExpensesBlock = function() {
+addExpensesBlock() {
   let expensesItemClone = expensesItems[0].cloneNode(true);
   expensesItems[0].parentNode.insertBefore(expensesItemClone, expensesBtn);
   expensesItems = document.querySelectorAll(".expenses-items");
   if (expensesItems.length === 3) {
     expensesBtn.style.display = "none";
   }
-};
+}
 
 // метод для записи в поля "Наименование" и "Сумма" в обязательных расходах
-AppData.prototype.getExpenses = function() {
-  expensesItems.forEach((item) => {
+getExpenses() {
+  expensesItems.forEach(item => {
     let itemExpenses = item.querySelector(".expenses-title").value;
     let cashExpenses = item.querySelector(".expenses-amount").value;
     if (itemExpenses !== "" && cashExpenses !== "") {
       this.expenses[itemExpenses] = +cashExpenses;
     }
   });
-};
-
+}
 
 //цель за месяц
-AppData.prototype.getTargetMonth = function() {
+getTargetMonth() {
   return targetAmount.value / this.budgetMonth;
-};
+}
 
 //условия для проверки уровня доходов
-AppData.prototype.getStatusIncome = function() {
+getStatusIncome() {
   if (this.budgetDay === 0) {
     return "Пора в жизни что-то менять!";
   } else if (this.budgetDay >= 1200) {
@@ -199,10 +204,10 @@ AppData.prototype.getStatusIncome = function() {
   } else if (this.budgetDay < 0) {
     return "Что-то пошло не так!";
   }
-};
+}
 
 //условия для рассчёта цели по времени(в месяцах)
-AppData.prototype.resultTargetMonth = function() {
+resultTargetMonth() {
   if (this.getTargetMonth() < 0) {
     return "Цель не будет достигнута";
   } else if (this.getTargetMonth() > 0) {
@@ -212,10 +217,10 @@ AppData.prototype.resultTargetMonth = function() {
       " месяцев!"
     );
   }
-};
+}
 
-
-AppData.prototype.getInfoDeposit = function() {
+//депозит
+getInfoDeposit() {
   while (
     !isNumber(this.percentDeposit) ||
     this.percentDeposit === 0 ||
@@ -231,14 +236,15 @@ AppData.prototype.getInfoDeposit = function() {
   ) {
     this.moneyDeposit = +prompt("Какая сумма заложена?", "");
   }
-};
+}
 
 // метод для рассчёта результата
-AppData.prototype.calcSavedMoney = function() {
+calcSavedMoney() {
   return this.budgetMonth * periodSelect.value;
-};
+}
 
-AppData.prototype.reset = function() {
+// сброс к первоначальному виду
+reset() {
   this.income = {};
   this.incomeMonth = 0;
   this.addIncom = [];
@@ -277,10 +283,10 @@ AppData.prototype.reset = function() {
     }
     expensesBtn.style.display = "block";
   }
-};
+ }
 
-AppData.prototype.eventsListeners = function() {
-
+// обработчики событий
+     eventsListeners() {
   calculate.addEventListener("click", () => {
     this.start();
   });
@@ -300,96 +306,8 @@ AppData.prototype.eventsListeners = function() {
   periodSelect.addEventListener("input", () => {
     periodAmount.value = this.calcPeriod();
   });
-};
+ }
+}
 
 const appData = new AppData();
 appData.eventsListeners();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//========================================================
-
-class CarWash {
-  constructor(brand, model = CarWash.noCarBaseModel(), services = []) {
-    this.brand = brand;
-    this.model = model;
-    this.washed = false; // это для работы с методом нашего класса. Тут я не передавал аргументы в конструктор
-    this._services = services;
-  }
-
-  static noCarBaseModel() {
-    return 'none';
-  }
-
-  // вот тут будет метод для нашего конструктора в классе.
-  // будет менять свойство в конструкторе washed  на true
-  washReady() {
-    this.washed = true;
-    CarWash.counter++;
-    this.report();
-  }
-
-  // вот тут будет вызываться оповещение с инфой, что всё готово.
-  report() {
-    console.log(this.brand, this.model, this.washed);
-  }
-
-  get services() {
-    console.log(this._services);
-    
-   return this._services.length ? 'Есть доп.услуги': 'Нет доп.услуг'; 
-  }
-
-  set services(addServices) {
-    return this._services.push(addServices);
-  }
-
-}
-// наследование от нашего класса CarWash
-class PassCar extends CarWash {
-  constructor(brand, model, services, pass = 5) { // конструктор с новым параметром
-    super(brand, model, services); // с помощью ф-ии super мы можем наследовать всё от нашего родительского класса
-    this.pass = pass;
-  }
-
-  washReady() {
-    super.washReady(); // и тут тоже
-    this.reportOffice(); // 2. тут мы вызываем наш новый метод, определённы в этом классе-наследнике
-  }
-
- // 1. это метод для нашего класса-наследника
-  reportOffice() {
-    console.log('На мойке для легковых была помыта машина');
-    
-  }
-}
-
-CarWash.counter = 0;
-
-let car1 = new CarWash('Mazda', 6, ['black tires', 'wax']);
-let car2 = new PassCar('Audi', 'A7');
-
-car2.services = 'Протирка стёкол';
-
-car1.washReady();
-car2.washReady();
-
-console.log(car1);
-console.log(car2);
-
-
-
-
