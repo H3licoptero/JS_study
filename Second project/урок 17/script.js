2c8be8f5bd2de702d05147de90864b80ba489087
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-02-20 02:50:09 +0300

    ДЗ к семнадцатому уроку(полторы шт)

--- Second project/урок 17/script.js
+++ Second project/урок 17/script.js
@@ -7,17 +7,17 @@ let date = new Date();
 let timeOfDay = date.getHours();
 
 let timeOfDayFunc = () => {
-  if (timeOfDay > 6 && timeOfDay < 12) {
-    console.log("Доброе утро!");
-  } else if (timeOfDay > 12 && timeOfDay < 18) {
-    console.log("Доброе утро!");
-  } else if (timeOfDay > 18 && timeOfDay < 22) {
-    console.log("Добрый вечер!");
-  } else if (timeOfDay > 22 || timeOfDay < 6) {
-    console.log("Доброй ночи!");
+  if (timeOfDay >= 6 && timeOfDay < 12) {
+    return ("Доброе утро!");
+  } else if (timeOfDay >= 12 && timeOfDay < 18) {
+    return ("Добрый день!");
+  } else if (timeOfDay >= 18 && timeOfDay < 22) {
+    return ("Добрый вечер!");
+  } else if (timeOfDay >= 22 || timeOfDay < 6) {
+    return ("Доброй ночи!");
   }
+  return timeOfDay;
 };
-// timeOfDayFunc();
 
 // создаем массив с днями недели (обращаем внимание на ключ!!!, он должен)
 // соответствовать тому, что передаст метод getData()
@@ -35,22 +35,12 @@ let days = [
 // с помощью метода getDay()
 let daysOfWeek = date.getDay();
 
-// наш массив будет получать значение метода через переменную daysOfWeek
-// в котором лежит наша дата и выводить на страницу
-// alert(`Сегодня: ${days[daysOfWeek]}`);
-
-// alert(`Текущее время: ${date.toLocaleTimeString('en')}`);
-
-
-let ourDay = date; 
-let happyDay = new Date(2020, 11, 31);
-
-let diff = Math.ceil((happyDay - ourDay) / (1000 * 60 * 60 * 24));
-
-// alert(`До Нового Года осталось: ${diff} дней!`);
+let ourDay = date,
+ happyDay = new Date(2020, 11, 31),
+ diff = Math.ceil((happyDay - ourDay) / (1000 * 60 * 60 * 24));
 
 alert(`${timeOfDayFunc()}
-Сегодня: ${days[daysOfWeek]}
+Сегодня: ${days[daysOfWeek]}                    
 Текущее время: ${date.toLocaleTimeString("en")}
 До Нового Года осталось: ${diff} дней!`);
 
