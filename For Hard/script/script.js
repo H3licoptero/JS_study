b26585e7193c608863a590e47ea9ffaacfe2fc0a
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-03-10 19:01:08 +0300

    Усложнёнка ко второму уроку

--- For Hard/script/script.js
+++ For Hard/script/script.js
@@ -1,5 +1,7 @@
 'use strict';
  
+
+// через reduce
 let num = 266219,
   count = String(num).split(''),
   sort = count.reduce((multiple, value) => multiple * value);
@@ -8,4 +10,27 @@ let num = 266219,
 let result = sort ** 3,
   firstNumbers = String(result).substr(0,2);
 
-console.log(firstNumbers);
+console.log(+firstNumbers);
+
+// через цикл for
+let num2 = 266219;
+let arr = String(num2).split('');
+let countValue,
+   sumNumbers,
+   multipleNumbers,
+   resultCount;
+
+for(let i = 0; i < arr.length; i++) {
+    countValue = arr[i];
+    arr[0] *= countValue;
+    sumNumbers = arr[0] / 2;
+
+    multipleNumbers = sumNumbers ** 3;    
+    resultCount = String(multipleNumbers).substr(0, 2);
+}
+
+console.log(sumNumbers);
+console.log(+resultCount);
+
+
+
